f35ec89f1107cd42152583c07a2c9ecf07a8f3f2
somemorewater <oboyiloejeh@gmail.com>
2026-02-19 02:59:34 +0100

    feat: add new files

---
+++ src/controllers/auth.js
@@ -0,0 +1,18 @@
+import User from "../models/User.js";
+import { signupSchema } from "../validations/auth.js";
+
+export const signupController = async (req, res) => {
+  try {
+    const validatedData = signupSchema.parse(req.body);
+
+    res.status(200).json({
+      message: "Validation passed",
+      data: validatedData,
+    });
+  } catch (err) {
+    if (err.name === "ZodError") {
+      return res.status(400).json({ errors: err.errors });
+    }
+    res.status(500).json({ error: "Server error" });
+  }
+};
