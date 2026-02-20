4c79562fa17fefb271b23990154f7ca5b4306914
somemorewater <oboyiloejeh@gmail.com>
2026-02-20 19:36:04 +0100

    feat: route auth controllers

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { signupController } from "../controllers/auth.js";
-import { signupSchema } from "../validations/auth.js";
+import {
+  signupController,
+  verifyOtpController,
+  loginController,
+} from "../controllers/auth.js";
 
 const router = express.Router();
 
+// Signup
 router.post("/signup", signupController);
+
+// Verify OTP
+router.post("/verify-otp", verifyOtpController);
+
+// Login
+router.post("/login", loginController);
 
 export default router;
