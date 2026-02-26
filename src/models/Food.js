48368b3e8cb84b98cd106f584adefd0ec9cfaaa7
somemorewater <oboyiloejeh@gmail.com>
2026-02-26 01:40:00 +0100

    feat: add food model

---
+++ src/models/Food.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const foodSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Food = mongoose.model("Food", foodSchema);
+
+export default Food;
