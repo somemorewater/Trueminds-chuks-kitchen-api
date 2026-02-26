eaad1cc6a78477bd8bb134c06a296d30ef591931
somemorewater <oboyiloejeh@gmail.com>
2026-02-26 01:42:18 +0100

    feat: add new routes for food, cart, and orders

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import express from "express";
 import authRoutes from "./routes/auth.js";
+import foodRoutes from "./routes/food.js";
+import cartRoutes from "./routes/cart.js";
+import orderRoutes from "./routes/order.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/foods", foodRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/orders", orderRoutes);
 
 export default app;
